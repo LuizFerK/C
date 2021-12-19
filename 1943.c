d0381ea1cbb5d4018b4a9af5bd7661d3300e54fa
LuizFerK <luizfernandoklein@live.com>
2021-12-18 21:41:37 -0300

    1943

---
+++ 1943.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main() {
+  int x;
+
+  scanf("%d", &x);
+
+  if (x <= 1) {
+    printf("Top 1\n");
+  } else if (x <= 3) {
+    printf("Top 3\n");
+  } else if (x <= 5) {
+    printf("Top 5\n");
+  } else if (x <= 10) {
+    printf("Top 10\n");
+  } else if (x <= 25) {
+    printf("Top 25\n");
+  } else if (x <= 50) {
+    printf("Top 50\n");
+  } else if (x <= 100) {
+    printf("Top 100\n");
+  }
+
+  return 0;
+}
